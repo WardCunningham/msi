a2676d3bc0519072a2ffffd08f7eaf2ef56fc811
Ward Cunningham <ward@c2.com>
2012-06-05 19:33:42 -0700

    page description dsl

--- folded-pages.rb
+++ folded-pages.rb
@@ -21,8 +21,8 @@ end
 
 # Load json from excel
 
-@table = {}
-@material = {}
+@tables = {}
+@materials = {}
 
 def load filename
   JSON.parse(File.read(filename))
@@ -49,26 +49,24 @@ def index key, table
 end
 
 def materials
-  @material['Tier1MSISummary'].keys.sort
+  @materials['Tier1MSISummary'].keys.sort
 end
 
 def name material
-  @material['Tier1MSISummary'][material]['Material']
+  @materials['Tier1MSISummary'][material]['Material']
 end
 
 def init
   Dir.glob('try9/*.json') do |filename|
     (prefix, table, sufix) = filename.split /[\/\.]/
-    @table[table] = input = load(filename)
+    @tables[table] = input = load(filename)
     convert! input
-    @material[table] = index input['columns'].first, input if (40..50).include? input['data'].length
+    @materials[table] = index input['columns'].first, input if (40..50).include? input['data'].length
     puts "#{table.ljust 30} #{input['data'].length} rows x #{input['columns'].length} columns (#{input['columns'].first})"
-    puts input['columns']
-    puts
   end
 end
 
-# Emit pages for federated wiki
+# wiki utilities
 
 def random
   (1..16).collect {(rand*16).floor.to_s(16)}.join ''
@@ -86,83 +84,135 @@ def url text
   text.gsub(/(http:\/\/)?([a-zA-Z0-9._-]+?\.(net|com|org|edu)(\/[^ )]+)?)/,'[http:\/\/\2 \2]')
 end
 
+# journal actions
+
 def create title
   {'type' => 'create', 'id' => random, 'item' => {'title' => title}}
 end
 
+# story emiters
+
 def paragraph text
-  {'type' => 'paragraph', 'text' => text, 'id' => random()}
+  @story << {'type' => 'paragraph', 'text' => text, 'id' => random()}
 end
 
 def data table, caption
-  {'type' => 'data', 'text' => caption, 'columns' => table['columns'], 'data' => table['data'], 'id' => random()}
+  @story << {'type' => 'data', 'text' => caption, 'columns' => table['columns'], 'data' => table['data'], 'id' => random()}
 end
 
 def fold text
-  {'type' => 'pagefold', 'text' => text, 'id' => random()}
+  @story << {'type' => 'pagefold', 'text' => text, 'id' => random()}
+  yield
 end
 
-def page title, story
-  page = {'title' => title, 'story' => story, 'journal' => [create(title)]}
+def page title
+  @story = []
+  yield
+  page = {'title' => title, 'story' => @story, 'journal' => [create(title)]}
   File.open("../pages/#{slug(title)}", 'w') do |file|
     file.write JSON.pretty_generate(page)
   end
 end
 
-def summary
-  story = []
-  story << data(@table['Tier1MSISummary'], "Tier1 MSI Summary")
-  materials.each do |material|
-    story << paragraph("[[#{name material}]]")
-  end
-  page 'Tier1 MSI Summary', story
+# custom emiters
+
+@record = nil
+
+def table name
+  @table = @materials[name]
+  yield
 end
 
-def say story, row, keys
-  keys.each do |key|
-    value = row[key].my_value
-    story << paragraph(value) unless value.empty?
+def record title
+  @record = {}
+  yield
+  data({'columns' => @record.keys, 'data' => [@record]}, title)
+  @record = nil
+end
+
+def dataset title
+  @dataset = materials.inject({}) { |hash, key| hash[key]={'Material' => key}; hash }
+  yield
+  values = @dataset.values
+  data({'columns' => values.first.keys, 'data' => values}, title)
+  @dataset = nil
+end
+
+def field column
+  return @dataset.each {|key,value| value[column] = @table[key][column]||''} if !@dataset.nil?
+  row = @table[@material]
+  return @record[column] = row[column] if !@record.nil?
+  value = row.nil? ? "N/A" : row[column].my_value
+  paragraph value unless value.empty?
+end
+
+# content generators
+
+def summary
+  page 'Tier1 MSI Summary' do
+    dataset 'Tier1 MSI Summary' do
+      table 'Tier1MSISummary' do
+        field 'Material'
+        field 'Total Score'
+        field 'Rank'
+      end
+    end
+    materials.each do |material|
+      paragraph "[[#{name material}]]"
+    end
   end
 end
 
 def content
-  # schema = [
-  #   {:table => 'Tier3MaterialData', :fields => [
-  #     'Nike MSI Supply Chain Scenario',
-  #     'Geographic Location','Data Sources',
-  #     'Production Method',
-  #     'Raw Material Factor',
-  #     'Data Quality Assessment']}
-  #   {:fold => {:name => 'chemistry', :story => [
-  #     {:table => 'Tier3MaterialData', :fields => [
-  #       'Chemistry Exposure Assumptions']}
-  #     ]}
-  #   {:fold => {:name => 'energy/ghg', :story => [
-  #     {:table => 'Tier3MaterialData', :fields => [
-  #       'Chemistry Exposure Assumptions']}
-  #     ]}
-  #   ]
-
   materials.each do |material|
-    story = []
-    if row = @material['Tier3MaterialData'][material]
-      say story, row, ['Nike MSI Supply Chain Scenario','Geographic Location','Data Sources','Production Method','Raw Material Factor','Data Quality Assessment']
+    @material = material
+    page name(material) do
+      record "Material Summary" do
+        table 'Tier1MSISummary' do
+          field 'Material'
+          field 'Total Score'
+          field 'Energy/GHG Emissions Intensity Total'
+          field ' Chemistry Total'
+          field 'Water/Land Intensity Total'
+          field 'Physical Waste Total'
+        end
+      end
+      table 'Tier3MaterialData' do
+        field 'Nike MSI Supply Chain Scenario'
+        field 'Geographic Location'
+        field 'Data Sources'
+        field 'Production Method'
+        field 'Raw Material Factor'
+        field 'Data Quality Assessment'
+      end
+      table'Tier3Materials' do
+        field 'Material Notes'
+        field 'Material Sources'
+      end
+
+      fold 'chemistry' do
+        table 'Tier3MaterialData' do
+          field 'Chemistry Exposure Assumptions'
+        end
+      end
+
+      fold 'energy/ghg' do
+        record "Energy/GHG Intensity" do
+          table 'Tier1MSIRawData' do
+            field 'Energy Intensity'
+            field 'GHG Emissions Intensity'
+          end
+        end
+        table 'Tier3MaterialData' do
+          paragraph 'Energy Scoring Drivers:'
+          field 'Energy Scoring Drivers Phase 1'
+          field 'Energy Scoring Drivers Phase 2'
+          paragraph 'GHG Emissions Scoring Drivers:'
+          field 'GHG Emissions Scoring Drivers Phase 1'
+          field 'GHG Emissions Scoring Drivers Phase 2'
+        end
+      end
     end
-    if row = @material['Tier3Materials'][material]
-      say story, row, ['Material Notes', 'Material Sources']
-    end
-    story << fold("chemistry")
-    if row = @material['Tier3MaterialData'][material]
-      say story, row, ['Chemistry Exposure Assumptions']
-    end
-    story << fold("energy/ghg")
-    if row = @material['Tier3MaterialData'][material]
-      story << paragraph("Energy Scoring Drivers")
-      say story, row, ['Energy Scoring Drivers Phase 1','Energy Scoring Drivers Phase 2']
-      story << paragraph("GHG Emissions Scoring Drivers")
-      say story, row, ['GHG Emissions Scoring Drivers Phase 1','GHG Emissions Scoring Drivers Phase 2']
-    end
-    page name(material), story
   end
 end
 
