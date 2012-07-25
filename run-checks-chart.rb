ad007ed30d339b4a013cf4943df74c46ab088a6e
Ward Cunningham <ward@c2.com>
2012-07-25 16:13:49 -0700

    run three scripts (one more) for new data

--- run-checks-chart.rb
+++ run-checks-chart.rb
@@ -5,7 +5,10 @@ try = Dir.glob('db/*-*-*').max_by {|e| File.mtime(e)}
 puts "Run for version #{try}."
 
 puts `ruby checks.rb > #{try}/Processed/trouble-checks.txt`
-puts `tail #{try}/Processed/trouble-checks.txt`
+puts "trouble-checks.txt: " + `tail -1 #{try}/Processed/trouble-checks.txt`
 
 puts `ruby formula-chart.rb > #{try}/Processed/trouble-chart.txt`
-puts `tail #{try}/Processed/trouble-chart.txt`
+puts "trouble-chart.txt: " + `tail -1 #{try}/Processed/trouble-chart.txt`
+
+puts `ruby folded-pages.rb > #{try}/Processed/trouble-pages.txt`
+puts "trouble-pages.txt: " + `tail -1 #{try}/Processed/trouble-pages.txt`
